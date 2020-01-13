48ea8cf5cb596b2671900188d1c8bbbd59b445f5
Brian McClain <brianmmcclain@gmail.com>
2020-01-13 16:06:24 -0500

    Disable service discovery

--- src/main/java/io/spring/cloud/samples/fortuneteller/ui/Application.java
+++ src/main/java/io/spring/cloud/samples/fortuneteller/ui/Application.java
@@ -4,13 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
-@EnableDiscoveryClient
 @EnableCircuitBreaker
 public class Application {
 	
