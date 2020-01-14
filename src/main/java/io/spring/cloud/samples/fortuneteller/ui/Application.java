3cea8aed8c19c88855594175a2571ad5b0481639
Brian McClain <brianmmcclain@gmail.com>
2020-01-14 14:37:30 -0500

    Remove SCS integration

--- src/main/java/io/spring/cloud/samples/fortuneteller/ui/Application.java
+++ src/main/java/io/spring/cloud/samples/fortuneteller/ui/Application.java
@@ -3,17 +3,13 @@ package io.spring.cloud.samples.fortuneteller.ui;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
-@EnableCircuitBreaker
 public class Application {
 	
     @Bean
-    @LoadBalanced
     public RestTemplate restTemplate() {
       return new RestTemplate();
     }
--- src/main/java/io/spring/cloud/samples/fortuneteller/ui/services/fortunes/FortuneService.java
+++ src/main/java/io/spring/cloud/samples/fortuneteller/ui/services/fortunes/FortuneService.java
@@ -1,16 +1,11 @@
 package io.spring.cloud.samples.fortuneteller.ui.services.fortunes;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
-@EnableConfigurationProperties(FortuneProperties.class)
 public class FortuneService {
-
-    @Autowired
-    FortuneProperties fortuneProperties;
 
     @Autowired
     RestTemplate restTemplate;
